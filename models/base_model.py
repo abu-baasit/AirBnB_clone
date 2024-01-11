5caf925c443d4dbd10dd276449526c60d5a16070
root <abu-baasit>
2024-01-11 04:24:20 -0800

    basemodel update

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 import models
 import uuid
 from datetime import datetime
+from models.engine.file_storage import storage
 
 
 class BaseModel:
@@ -25,6 +26,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+        models.storage.new(self)
 
     def __str__(self):
         """Returns the string that represents the
